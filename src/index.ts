5cd7382baaa74a5a5e3d265348e4b7c40de0e424
torio <raikan327@icloud.com>
2024-10-20 10:59:08 +0900

    feat : create /menuList/:id api

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,14 @@ app.get('/',(c)=>{
   return c.text("Hello hono")
 })
 
+app.get('/menuList/:id',(c)=>{
+  const id:string=c.req.param('id')
+  if(Number(id)>menuList.length){
+    return c.json({
+      message:"Not found"
+    },404)
+  }
+  return c.json(menuList[Number(id)-1],201)
+})
+
 export default app
